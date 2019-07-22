9ef2a98f2d9dfb5786590b334a4888a3c8a4a095
Lucaci Andrei <lucaciandrei@outlook.com>
2019-07-22 17:22:46 +0300

    implemented first exercice

---
+++ src/components/ex1.js
@@ -0,0 +1,17 @@
+// Objective: Fill in the boilerplate Reat code required to render an unorder list <ul> to the page
+// The list should contain 3 list itmes (<li>) with anything that you want.
+
+import React from 'react';
+
+export default () => {
+    let items = [];
+
+    ["anything", "that", "i want"].forEach(x => items.push(<li>{x}</li>));
+
+    return (
+        <ul>
+            {items}
+        </ul>
+    );
+};
+
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Ex1 from './components/ex1';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<div><h1>Hello world</h1><p>This is a paragraph</p></div>, document.getElementById('root'));
+ReactDOM.render(<Ex1 />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
