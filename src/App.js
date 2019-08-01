280a8a4e82f738e44865ed2fbfc8f814f26c52b9
Lucaci Andrei <lucaciandrei@outlook.com>
2019-08-01 14:52:31 +0300

    added todo app

--- src/App.js
+++ src/App.js
@@ -3,24 +3,79 @@ import "./App.css";
 
 // import exercices
 
-import Footer from "./components/Footer/Footer";
-import Header from "./components/Header/Header";
-import MainContent from "./components/MainContent/MainContent";
+import Todos from "./components/todos/Todos";
+import Header from "./components/layout/Header";
+import AddTodo from "./components/todos/AddTodo";
+
+import uuid from "uuid";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-
   }
+
+  state = {
+    todos: [
+      {
+        id: uuid.v4(),
+        title: "Take ou the trash",
+        completed: false
+      },
+      {
+        id: uuid.v4(),
+        title: "Dinner with wife",
+        completed: true
+      },
+      {
+        id: uuid.v4(),
+        title: "Meeting with boss",
+        completed: false
+      }
+    ]
+  };
+
+  toggleComplete = id => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+
+        return todo;
+      })
+    });
+  };
+
+  addToDo = title => {
+	const newTodo = {
+		id: uuid.v4(),
+		title,
+		completed: false
+	};
+	
+	this.setState({
+		todos: [...this.state.todos, newTodo]
+	});
+  }
+
+  delTodo = id => {
+    this.setState({
+      todos: [...this.state.todos.filter(todo => todo.id !== id)]
+    });
+  };
 
   render() {
     return (
       <div className="App">
-        <Header />
-        <div className="App-body">
-          <MainContent />
+        <div className="container">
+          <Header />
+          <AddTodo addToDo={this.addToDo} />
+          <Todos
+            todos={this.state.todos}
+            toggleComplete={this.toggleComplete}
+            delTodo={this.delTodo}
+          />
         </div>
-        <Footer />
       </div>
     );
   }
